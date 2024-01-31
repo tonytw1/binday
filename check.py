c7a02ff9a07a9f669f4064f2c0e2eceaf6c2e8cc
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-01-31 21:26:48 +0000

    UPRN from ENV.

--- check.py
+++ check.py
@@ -1,9 +1,11 @@
+import os
 import requests
 from datetime import date
 from dateutil.parser import parse
 
-uprn = ''
+uprn = os.environ.get('UPRN')
 api_url = 'https://online.bcpcouncil.gov.uk/bcp-apis/?api=BinDayLookup&uprn=' + uprn
+print(api_url)
 
 def collected_tommorow(bin):
     today = date.today()
@@ -13,12 +15,16 @@ def collected_tommorow(bin):
     return delta == 1
 
 r = requests.get(api_url)
-if r.status_code == 200:
-    data = r.json()
-    tommorows_bins = list(filter(collected_tommorow, data))
-    if len(tommorows_bins) != 0:
-        types = list(map(lambda b: b['BinType'], tommorows_bins))
-        print(', '.join(types))
+if r.status_code != 200:
+    print(r.status_code)
+    exit()
+
+data = r.json()
+print(data)
+
+tommorows_bins = list(filter(collected_tommorow, data))
+if len(tommorows_bins) != 0:
+    types = list(map(lambda b: b['BinType'], tommorows_bins))
+    print(', '.join(types))
 
 print("Done")
-
