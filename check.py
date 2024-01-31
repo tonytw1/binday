9b58855c913e4d5c42c9c7a4270380b7cf36e84b
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-01-31 23:05:29 +0000

    Initial email send.

--- check.py
+++ check.py
@@ -2,10 +2,33 @@ import os
 import requests
 from datetime import date
 from dateutil.parser import parse
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 
 uprn = os.environ.get('UPRN')
+message_from = os.environ.get('EMAIL_FROM')
+message_to = os.environ.get('EMAIL_TO')
+smtp_user = os.environ.get('SMTP_USER')
+smtp_password = os.environ.get('SMTP_PASSWORD')
+smtp_server = os.environ.get('SMTP_HOST')
+
 api_url = 'https://online.bcpcouncil.gov.uk/bcp-apis/?api=BinDayLookup&uprn=' + uprn
 print(api_url)
+
+def send_email(text):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = message_from
+    message["To"] = message_to
+
+    plain_part = MIMEText(text, "plain")
+    message.attach(plain_part)
+
+    server = smtplib.SMTP(smtp_server, 587)
+    server.login(smtp_user, smtp_password)
+    server.sendmail(message_from, message_to, message.as_string())
 
 def collected_tommorow(bin):
     today = date.today()
@@ -25,6 +48,8 @@ print(data)
 tommorows_bins = list(filter(collected_tommorow, data))
 if len(tommorows_bins) != 0:
     types = list(map(lambda b: b['BinType'], tommorows_bins))
-    print(', '.join(types))
+    content = ', '.join(types)
+    print(content)
+    send_email(content)
 
 print("Done")
